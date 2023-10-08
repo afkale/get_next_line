e079610b80d9785ab9413471d7ce3f3300e06d30
Alejandro Rubio Oliva <arubio-o@c2r16s1.42madrid.com>
2023-10-08 10:52:27 +0200

    feat: static structs

--- get_next_line.c
+++ get_next_line.c
@@ -5,64 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: arubio-o <arubio-o@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/24 19:29:05 by arubio-o          #+#    #+#             */
-/*   Updated: 2023/09/24 20:14:19 by arubio-o         ###   ########.fr       */
+/*   Created: 2023/09/24 19:32:57 by arubio-o          #+#    #+#             */
+/*   Updated: 2023/10/07 15:36:15 by arubio-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "get_next_line.h"
 
-void	*ft_calloc(size_t number, size_t size)
+void	ft_read(void)
 {
-	size_t			i;
-	unsigned char	res;
+	int	fd;
 
-	i = 0;
-	res = malloc(size * number);
-	while (i < size)
-		res[i] = 0;
-	return (res);
+	fd = open("./test_short_line.txt", O_RDONLY);
+	g_buffer.size = read(fd, buffer.content, BUFFER_SIZE - 1);
 }
 
-void	*ft_memmove(void *dest, const void *src, size_t count)
+int	main(void)
 {
-	unsigned char	*dest_aux;
-	unsigned char	*src_aux;
-
-	dest_aux = (unsigned char *) dest;
-	src_aux = (unsigned char *) src;
-
-	if ((dest_aux + count) >= src)
-		while (count--)
-			*(dest_aux + count) = *(src_aux + count);
-	else
-		while (count--)
-			*dest_aux++ = *src_aux++;
-	return (dest);
+	printf("%s, %zu, %zu\n", g_buffer.content, g_buffer.size, g_buffer.last);
 }
 
-void	*ft_realloc(const void *memblock, size_t size)
-{
-	unsigned char	*output;
-	unsigned char	memblock;
-
-	output = ft_calloc(size);
-
-
-
-}
-
-char	*get_next_line(int fd)
-{
-
-
-
-}
-
-
-int	main()
-{
-
-
-
-}
+/*
+ * 
+ *
+ */
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,27 @@
 /*   By: arubio-o <arubio-o@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 19:32:57 by arubio-o          #+#    #+#             */
-/*   Updated: 2023/09/24 19:34:41 by arubio-o         ###   ########.fr       */
+/*   Updated: 2023/10/07 15:36:15 by arubio-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
 char	*get_next_line(int fd);
+
+static struct s_buffer {
+	char	content[BUFFER_SIZE];
+	size_t	size;
+	size_t	last;
+}	g_buffer = {"", 0, 0};
 
 #endif // !GET_NEXT_LINE_H
