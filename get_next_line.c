78443b720eef02c3aec2ec391be0adcfc3dc35b3
arubioliva <arubioo977@gmail.com>
2023-10-28 11:06:21 +0200

    feat: clean norminette

--- get_next_line.c
+++ get_next_line.c
@@ -14,56 +14,52 @@
 
 char	*strljoin(char *dest, char *src, size_t n)
 {
-    size_t	i;
-    char	*res;
+	size_t	i;
+	char	*res;
 
-    i = 0;
-    res = (char *) malloc(ft_strlen(dest) + n + 1);
-    if (res != NULL)
-    {
-        if (dest != NULL)
-        {
-            while (dest[i])
-            {
-                res[i] = dest[i];
-                i++;
-            }
-        }
-        while (n--)
-            res[i++] = *src++;
-        res[i] = 0;
-    }
-    ft_clear(&dest);
-    return (res);
+	i = 0;
+	res = (char *) malloc(ft_strlen(dest) + n + 1);
+	if (res != NULL)
+	{
+		if (dest != NULL)
+		{
+			while (dest[i])
+			{
+				res[i] = dest[i];
+				i++;
+			}
+		}
+		while (n--)
+			res[i++] = *src++;
+		res[i] = 0;
+	}
+	ft_clear(&dest);
+	return (res);
 }
 
 char	*get_next_line(int fd)
 {
-    size_t			endl;
-    char			*line;
-    static t_buffer	buffer;
+	size_t			endl;
+	char			*line;
+	static t_buffer	buffer;
 
-    line = NULL;
-    endl = 0;
-    if (fd <= 0 || BUFFER_SIZE <= 0)
-    {
-        buffer.last = 0;
-        buffer.count = 0;
-        return (NULL);
-    }
-    while (1)
-    {
-        if (buffer.last == 0)
-            buffer.count = read(fd, buffer.content, BUFFER_SIZE);
-        if (buffer.count <= 0)
-            return (line);
-        endl = strend(buffer);
-        line = strljoin(line, buffer.content + buffer.last, endl);
-        buffer.last += endl;
-        if (buffer.last != buffer.count)
-            return (line);
-        buffer.last = 0;
-        if(buffer.content[buffer.count - 1] == '\n')
-            return (line);
-    }
+	line = NULL;
+	endl = 0;
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buffer.last == 0)
+			buffer.count = read(fd, buffer.content, BUFFER_SIZE);
+		if (buffer.count <= 0)
+			return (line);
+		endl = strend(buffer);
+		line = strljoin(line, buffer.content + buffer.last, endl);
+		buffer.last += endl;
+		if (buffer.last != buffer.count)
+			return (line);
+		buffer.last = 0;
+		if (buffer.content[buffer.count - 1] == '\n')
+			return (line);
+	}
 }
--- tests.c
+++ tests.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   tests.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arubio-o <arubio-o@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: ale </var/spool/mail/ale>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/24 19:32:57 by arubio-o          #+#    #+#             */
-/*   Updated: 2023/10/15 19:17:49 by arubio-o         ###   ########.fr       */
+/*   Created: 2023/10/28 10:53:51 by ale               #+#    #+#             */
+/*   Updated: 2023/10/28 10:58:55 by ale              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,122 +14,119 @@
 #include <stdio.h>
 #include <string.h>
 
-int  test1(void)
+int	test1(void)
 {
-    char    *line;
-    char	out_filename[100];
-    char	in_filename[100];
-    int     fd;
-    int     fd2;
-    int     next;
-    size_t  i;
+	char		*line;
+	char		out_filename[100];
+	char		in_filename[100];
+	int			fd;
+	int			fd2;
+	int			next;
+	size_t		i;
+	const char	*files[] = {
+		"1char.txt",
+		"41_no_nl",
+		"41_no_nl.txt",
+		"41_with_nl",
+		"42_no_nl",
+		"42_with_nl",
+		"43_no_nl",
+		"43_with_nl",
+		"alternate_line_nl_no_nl",
+		"alternate_line_nl_with_nl",
+		"big_line_no_nl",
+		"big_line_with_nl",
+		"el_quijote_ol.txt",
+		"el_quijote.txt",
+		"empty",
+		"empty.txt",
+		"giant_line_nl.txt",
+		"giant_line.txt",
+		"hola.txt",
+		"lines_around_10.txt",
+		"multiple_line_no_nl",
+		"multiple_line_with_nl",
+		"multiple_nl.txt",
+		"multiple_nlx5",
+		"nl",
+		"one_line_no_nl.txt",
+		"only_nl.txt",
+		"read_error.txt",
+		"test2.txt",
+		"test_nl",
+		"test_short_line.txt",
+		"variable_nls.txt",
+		NULL
+	};
 
-    line = NULL;
-    i = 0;
-    char  *files[] = {
-        "1char.txt",
-        "41_no_nl",
-        "41_no_nl.txt",
-        "41_with_nl",
-        "42_no_nl",
-        "42_with_nl",
-        "43_no_nl",
-        "43_with_nl",
-        "alternate_line_nl_no_nl",
-        "alternate_line_nl_with_nl",
-        "big_line_no_nl",
-        "big_line_with_nl",
-        "el_quijote_ol.txt",
-        "el_quijote.txt",
-        "empty",
-        "empty.txt",
-        "giant_line_nl.txt",
-        "giant_line.txt",
-        "hola.txt",
-        "lines_around_10.txt",
-        "multiple_line_no_nl",
-        "multiple_line_with_nl",
-        "multiple_nl.txt",
-        "multiple_nlx5",
-        "nl",
-        "one_line_no_nl.txt",
-        "only_nl.txt",
-        "read_error.txt",
-        "test2.txt",
-        "test_nl",
-        "test_short_line.txt",
-        "variable_nls.txt",
-        NULL
-    };
-    while (files[i])
-    {
-        sprintf(in_filename, "files/%s", files[i]);
-        fd = open(in_filename, O_RDONLY);
-        sprintf(out_filename, "responses/%s", files[i]);
-
-        fd2 = open(out_filename, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-
-        printf("%zu: %s\n", i+1, in_filename);
-        while ((line = get_next_line(fd)))
-        {
-            write(fd2, line, ft_strlen(line));
-            ft_clear(&line);
-        }
-        close(fd);
-        close(fd2);
-        i++;
-    }
-    return (0);
+	line = NULL;
+	i = 0;
+	while (files[i])
+	{
+		sprintf(in_filename, "files/%s", files[i]);
+		fd = open(in_filename, O_RDONLY);
+		sprintf(out_filename, "responses/%s", files[i]);
+		fd2 = open(out_filename, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		printf("%zu: %s\n", i + 1, in_filename);
+		while ((line = get_next_line(fd)))
+		{
+			write(fd2, line, ft_strlen(line));
+			ft_clear(&line);
+		}
+		close(fd);
+		close(fd2);
+		i++;
+	}
+	return (0);
 }
 
-int test2(int argc, char *argv[])
+int	test2(int argc, char *argv[])
 {
-    int	fd;
-    char	*line;
-    int	line_number;
+	int		fd;
+	char	*line;
+	int		line_number;
 
-    line_number = 0;
-    line = NULL;
-    if (argc != 2)
-    {
-        printf("Te faltan los parametros");
-        return (1);
-    }
-    fd = open(argv[1], O_RDONLY);
-    while ((line = get_next_line(fd)))
-    {
-        printf("%d: %s", line_number++, line);
-        ft_clear(&line);
-    }
-    printf("%d: %s", line_number++, line);
-    close(fd);
-    printf("\n");
-    return (0);
+	line_number = 0;
+	line = NULL;
+	if (argc != 2)
+	{
+		printf("Te faltan los parametros");
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%d: %s", line_number++, line);
+		ft_clear(&line);
+	}
+	printf("%d: %s", line_number++, line);
+	close(fd);
+	printf("\n");
+	return (0);
 }
 
-int test3()
+int	test3(void)
 {
-    int	fd;
-    char	*line;
+	int		fd;
+	char	*line;
 
-    line = NULL;
-    fd = -1;
-    while ((line = get_next_line(fd)))
-    {
-        printf("%s", line);
-        ft_clear(&line);
-    }
-    fd = 100;
-    while ((line = get_next_line(fd)))
-    {
-        printf("%s", line);
-        ft_clear(&line);
-    }
+	line = NULL;
+	fd = -1;
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		ft_clear(&line);
+	}
+	fd = 100;
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		ft_clear(&line);
+	}
 }
-
 
 int	main(int argc, char *argv[])
 {
-    test1();
-    return (0);
+	test1();
+	return (0);
 }
